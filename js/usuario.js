4c10efe56c5b19e4afe44e9d48b0115deffae209
Dalma Cersosimo <cersosimodalma@gmail.com>
2023-09-07 18:30:10 -0300

    primer commit

---
+++ js/usuario.js
@@ -0,0 +1,14 @@
+
+const usuario = JSON. parse(localStorage.getItem("usuarios"));
+const form = document.querySelector("form");
+
+usuario[
+    {
+        id:1,
+        nombre:"",
+        apellido:"",
+        fechaElegida:"",
+        vacunas:"",
+    }
+]
+
